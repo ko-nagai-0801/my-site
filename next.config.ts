d521c5a6228ee4aec4eece8fddf7d44a1c8bfaa7
koh <ko.nagai.0801@gmail.com>
2026-01-21 09:23:37 +0900

    posts/linebreak を frontmatter 形式へ統一し、非表示メモは frontmatter に持たせる方針にする / MDXのfrontmatterをmetaとして扱えるように設定追加

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,31 @@
+// next.config.ts
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
 
+import remarkGfm from "remark-gfm";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
 const withMDX = createMDX({
-  // これがあると .mdx だけを対象にできます（無くても動くことは多い）
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkGfm],
-    // rehypePlugins: [], // 必要になったら追加
+    remarkPlugins: [
+      remarkGfm,
+      remarkFrontmatter,
+      [remarkMdxFrontmatter, { name: "meta" }], // frontmatter -> export const meta
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+    ],
   },
 });
-
 
 export default withMDX(nextConfig);
